86b88a6a6ccaa96b3c79ffa503f603e765352791
Jaider bautista Rodriguez <117297163+Jaiderbr@users.noreply.github.com>
2024-10-09 18:59:26 -0500

    Update segment whit the maximum sum

--- Utilities/segment whit the maximum sum.cpp
+++ Utilities/segment whit the maximum sum.cpp
@@ -2,19 +2,22 @@
 segment whit the maximum sum    
 add to segment tree the node struct
 T neutro = T();
-T oper(T a, T b) {T res; return res.get(a, b);}
+T oper(T a, T b) {node::get(a, b);}
+Check the base ans
 */
+constexpr int inf=(1e18);
 struct node {
     int lt, rt, sum, ans;
-    node() : lt(0), rt(0), sum(0), ans(0) {}
+    node() : lt(-inf), rt(-inf), sum(0), ans(-inf) {}
     node(int x) : sum(x) {
-        lt = rt = ans = max(0ll, x);
+        lt = rt = ans = (x);
     }
-    node get(node& a, node& b) {
-        sum = a.sum + b.sum;
-        lt = max(a.lt, a.sum + b.lt);
-        rt = max(b.rt, b.sum + a.rt);
-        ans = max({ a.ans, b.ans, a.rt + b.lt });
-        return *this;
+    static node get(node &a, node&b) {
+        node res;
+        res.sum = a.sum + b.sum;
+        res.lt = max(a.lt, a.sum + b.lt);
+        res.rt = max(b.rt, b.sum + a.rt);
+        res.ans = max({ a.ans, b.ans, a.rt + b.lt });
+        return res;
     }
 };
