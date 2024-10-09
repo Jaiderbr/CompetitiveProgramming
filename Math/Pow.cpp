2ae22983b16e8f153a4238d3c100378b1ce86f77
Jaider bautista Rodriguez <117297163+Jaiderbr@users.noreply.github.com>
2024-10-08 23:11:51 -0500

    Update Pow

--- Math/Pow.cpp
+++ Math/Pow.cpp
@@ -1,6 +1,6 @@
-long long binpow(long long a, long long b, long long m) {
+int64_t binpow(int64_t a, int64_t b, int64_t m) {
     a %= m;
-    long long res = 1;
+    int64_t res = 1;
     while (b > 0) {
         if (b & 1)
             res = res * a % m;
@@ -10,8 +10,8 @@ long long binpow(long long a, long long b, long long m) {
     return res;
 }
 
-long long binpow(long long a, long long b) {
-    long long res = 1;
+int64_t binpow(int64_t a, int64_t b) {
+    int64_t res = 1;
     while (b > 0) {
         if (b & 1)
             res = res * a;
