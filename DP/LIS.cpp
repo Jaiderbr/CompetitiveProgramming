1d30b95f49eef8fe8dd5b2e519e8471363d30dca
Jaider bautista Rodriguez <jaiderbautista321@gmail.com>
2024-11-03 21:10:16 -0500

    ‏‏‎‎

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -8,3 +8,22 @@ int lis(vector<int>& a) {
     return sz(dp);
 
 }
+
+
+int lis_2(int n, vector<int>& A, vector<int>& B) {
+
+    vector<vector<int>>C(inf, vector<int>());
+    reverse(all(A)); reverse(all(B));
+    for (int i = n - 1; i >= 0; i--) C[B[i]].pb(i);
+
+    STree<int> dp(n); // Stree de max, neutro = INT_MIN
+
+    for (int j = 0; j < n; j++) {
+        for (auto&& i : C[A[j]]) {
+            int mx_pre = dp.query(0, i);
+            if (mx_pre != INT_MIN) dp.upd(i, mx_pre + 1);
+            else dp.upd(i, 1);
+        }
+    }
+    return dp.query(0, n);
+}
