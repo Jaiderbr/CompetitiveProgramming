6a14f47afe5b557896812580bfb7c344a6059740
Jaider bautista Rodriguez <jaiderbautista321@gmail.com>
2025-01-10 23:04:35 -0500

    ‎

--- Utilities/util.cpp
+++ Utilities/util.cpp
@@ -5,33 +5,33 @@ __builtin_ctz(x);       // Cuenta el numero de bits en '0' a la derecha, desde e
 __builtin_ffs(x);       // Encuentra la posicion del primer bit en '1' (contando desde 1, desde el bit menos significativo).
 __lg(x);                // Devuelve el logaritmo en base 2
 n & ~(1 << (x - 1));    // Apaga el m-esimo bit de n (bit 1 si m=1 es el menos significativo), Si m=1, apaga el bit menos significativo.
-x &(-x);                // Aisla el bit menos significativo en '1' de x (devuelve el bit mas bajo en '1' de x).
+x& (-x);                // Aisla el bit menos significativo en '1' de x (devuelve el bit mas bajo en '1' de x).
 ~x & (x + 1);           // Aisla el bit menos significativo en '0' de x.
 x | (x + 1);            // Enciende el bit menos significativo en '0' de x.
-x &(x - 1);             // Apaga el bit menos significativo en '1' de x.
+x& (x - 1);             // Apaga el bit menos significativo en '1' de x.
 -~n;                    // Suma 1 a n. 
 ~- n;                   // Resta 1 a n.
-x && (!(x & (x - 1)));  // Comprueba si x es una potencia de 2. 
+x && (!(x& (x - 1)));  // Comprueba si x es una potencia de 2. 
 
 //Rotar una matriz 90 grados
 int n;
-vector<vector<int>> rotar(vector<vector<int>> &a) {
+vector<vector<int>> rotar(vector<vector<int>>& a) {
   vector<vector<int>> v(n, vi(n));
-  forn(i,n) forn(j, n)
+  forn(i, n) forn(j, n)
     v[i][j] = a[n - 1 - j][i];
   return v;
 }
 
 //1234567891011121314151617... what is the digit at position n?
 char digit_at_pos(int n) {
-    n--; // 0 index
-    int len = 9, mm = 1;
-    forne(i, 1, 32) {
-        if (n < len) {
-            int num = n / i + mm, pos = n % i;
-            return to_string(num)[pos];
-        }
-        n -= len, mm *= 10, len = 9 * mm * (i + 1);
+  n--; // 0 index
+  int len = 9, mm = 1;
+  forne(i, 1, 32) { // change 32 to 64 if needed
+    if (n < len) {
+      int num = n / i + mm, pos = n % i;
+      return to_string(num)[pos];
     }
+    n -= len, mm *= 10, len = 9 * mm * (i + 1);
+  }
 }
 
