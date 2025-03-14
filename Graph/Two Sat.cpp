9cb5d8054e476ddb0f2c9813be2b1e6163a5d0b7
Jaider bautista Rodriguez <117297163+Jaiderbr@users.noreply.github.com>
2025-03-13 23:04:57 -0500

    ‎

--- Graph/Two Sat.cpp
+++ Graph/Two Sat.cpp
@@ -1,49 +1,65 @@
-struct two_sat {
-  int n, cont, scc;
-  vector<vector<int>> ady;
-  vector<int> low, num, comp, val;
-  stack<int> st;
-  const int INF = int(1e9);
+struct sat {
+	int n, tot;
+	vector<vector<int>> g;
+	vector<int> vis, comp, id, ans;
+	stack<int> s;
 
-  two_sat(int n): n(n), cont(0), scc(0), ady(n << 1), low(n << 1), num(n << 1, -1), comp(n << 1), val(n) {}
+	sat() {}
+	sat(int n_) : n(n_), tot(n), g(2*n) {}
 
-  void add_edge(int v, int u) {
-    ady[get_inx(-v)].pb(get_inx(u));
-    ady[get_inx(-u)].pb(get_inx(v));
-  }
+	int dfs(int i, int& t) {
+		int lo = id[i] = t++;
+		s.push(i), vis[i] = 2;
+		for (int j : g[i]) {
+			if (!vis[j]) lo = min(lo, dfs(j, t));
+			else if (vis[j] == 2) lo = min(lo, id[j]);
+		}
+		if (lo == id[i]) while (1) {
+			int u = s.top(); s.pop();
+			vis[u] = 1, comp[u] = i;
+			if ((u>>1) < n and ans[u>>1] == -1) ans[u>>1] = ~u&1;
+			if (u == i) break;
+		}
+		return lo;
+	}
 
-  int get_inx(int v) {
-    return ((abs(v) - 1) << 1) | (v < 0);
-  }
+	void add_impl(int x, int y) { // x -> y = !x ou y
+		x = x >= 0 ? 2*x : -2*x-1;
+		y = y >= 0 ? 2*y : -2*y-1;
+		g[x].push_back(y);
+		g[y^1].push_back(x^1);
+	}
+	void add_cl(int x, int y) { // x ou y
+		add_impl(~x, y);
+	}
+	void add_xor(int x, int y) { // x xor y
+		add_cl(x, y), add_cl(~x, ~y);
+	}
+	void add_eq(int x, int y) { // x = y
+		add_xor(~x, y);
+	}
+	void add_true(int x) { // x = T
+		add_impl(~x, x);
+	}
+	void at_most_one(vector<int> v) { // no max um verdadeiro
+		g.resize(2*(tot+v.size()));
+		for (int i = 0; i < v.size(); i++) {
+			add_impl(tot+i, ~v[i]);
+			if (i) {
+				add_impl(tot+i, tot+i-1);
+				add_impl(v[i], tot+i-1);
+			}
+		}
+		tot += v.size();
+	}
 
-  void tarjan(int v) {
-    low[v] = num[v] = cont++;
-    st.push(v);
-    for (int &u : ady[v]) {
-      if (num[u] == -1) tarjan(u);
-      low[v] = min(low[v], low[u]);
-    }
-    if (low[v] == num[v]) {
-      int u;
-      do {
-        u = st.top(); st.pop();
-        low[u] = INF;
-        comp[u] = scc;
-      } while (u != v);
-      scc++;
-    }
-  }
-
-  bool check() {
-    for (int i = 0; i < (n << 1); ++i) {
-      if (num[i] == -1) {
-        tarjan(i);
-      }
-    }
-    for (int i = 0; i < n; ++i) {
-      if (comp[i << 1] == comp[(i << 1) | 1]) return false;
-      val[i] = comp[i << 1] < comp[(i << 1) | 1];
-    }
-    return true;
-  }
+	pair<bool, vector<int>> solve() {
+		ans = vector<int>(n, -1);
+		int t = 0;
+		vis = comp = id = vector<int>(2*tot, 0);
+		for (int i = 0; i < 2*tot; i++) if (!vis[i]) dfs(i, t);
+		for (int i = 0; i < tot; i++)
+			if (comp[2*i] == comp[2*i+1]) return {false, {}};
+		return {true, ans};
+	}
 };
