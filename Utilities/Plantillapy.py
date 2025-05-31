139b98c7b72a773000483eaa80fecd748432dd8e
Jaider bautista Rodriguez <117297163+Jaiderbr@users.noreply.github.com>
2025-05-31 10:57:25 -0500

    ‎

---
+++ Utilities/Plantillapy.py
@@ -0,0 +1,12 @@
+import math
+import sys
+input = sys.stdin.readline
+write = sys.stdout.write
+def fast_print(x): write(str(x) + '\n')
+
+def main():
+
+    fast_print("Hello!!")
+
+if __name__ == '__main__':
+    main()
