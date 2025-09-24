ef391a15d5b8558712e6319199c62c29b286e38f
Jaider bautista Rodriguez <jaiderbautista321@gmail.com>
2025-09-24 15:17:42 -0500

    ­

--- Utilities/int128.cpp
+++ Utilities/int128.cpp
@@ -1,6 +1,13 @@
 using lint = __int128;
+static inline lint abs128(lint x) { return x < 0 ? -x : x; }
+static inline lint gcd128(lint a, lint b) {
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    while (b) { lint t = a % b; a = b; b = t; }
+    return a;
+}
 
-istream& operator>>(istream &in, lint &x) {
+istream& operator>>(istream& in, lint& x) {
     string s; in >> s;
     x = 0; bool neg = 0; int i = 0;
     if (s[0] == '-') neg = 1, i = 1;
@@ -9,7 +16,7 @@ istream& operator>>(istream &in, lint &x) {
     return in;
 }
 
-ostream& operator<<(ostream &out, lint x) {
+ostream& operator<<(ostream& out, lint x) {
     if (x == 0) return out << "0";
     if (x < 0) out << '-', x = -x;
     string s;
