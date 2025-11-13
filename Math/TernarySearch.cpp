a60a399032034a862e7186951bf90726a862b67a
Jaider Bautista Rodriguez <117297163+Jaiderbr@users.noreply.github.com>
2025-11-12 23:26:32 -0500

    Refactor ternary search function and clean up code

--- Math/TernarySearch.cpp
+++ Math/TernarySearch.cpp
@@ -1,20 +1,18 @@
 
-double f(double x) {
-    return x;
-}
-// ternary_search(0.0, posibleMaximo)
-double ternary_search(double l, double r) {
+double f(double x) { return x; }
+
+double t_search(double l, double r) {
     double eps = 1e-9;
+    // for(iter,100) {}
     while (r - l > eps) {
         double m1 = l + (r - l) / 3;
         double m2 = r - (r - l) / 3;
-        double f1 = f(m1);
-        double f2 = f(m2);
-        // if (f1 > c) f1 = f2 minimizar;
+        double f1 = f(m1), f2 = f(m2);        
         if (f1 < f2) l = m1;
         else r = m2;
     }
 
+    // l = (l + r) / 2; //precisamos el punto medio?
     // return l; 
     return f(l);
 }
